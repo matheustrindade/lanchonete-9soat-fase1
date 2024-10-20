63973f0929b853e47b7f2cd5fdc7f11bc1cf53d4
Eder Matos <ederfmatos@gmail.com>
2024-10-20 16:26:34 -0300

    feature: Add product to shopping cart

---
+++ src/infra/repository/ShoppingCartRepository.ts
@@ -0,0 +1,17 @@
+import { ShoppingCartRepository } from "@/application/repository/ShoppingCart";
+import { ShoppingCart } from "@/domain/entity/ShoppingCart";
+import { Collection } from "mongodb";
+
+export class ShoppingCartMongoRepository implements ShoppingCartRepository {
+  constructor(private readonly collection: Collection) {}
+
+  async save(shoppingCart: ShoppingCart): Promise<void> {
+    await this.collection.updateOne({ customerId: shoppingCart.customerId }, { $set: shoppingCart }, { upsert: true });
+  }
+
+  async findByCustomerId(customerId: string): Promise<ShoppingCart | null> {
+    const shoppingCart = await this.collection.findOne<any>({ customerId });
+    if (!shoppingCart) return null;
+    return ShoppingCart.restore(shoppingCart.customerId, shoppingCart.totalPrice, shoppingCart.items);
+  }
+}
