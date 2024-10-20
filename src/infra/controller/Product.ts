ee13331064c5119c270534b08e2903ce5e41f543
Eder Matos <ederfmatos@gmail.com>
2024-10-20 15:46:49 -0300

    feature: List products

--- src/infra/controller/Product.ts
+++ src/infra/controller/Product.ts
@@ -4,36 +4,43 @@ import { UpdateProductUseCase } from "@/application/usecase/UpdateProduct";
 import { DeleteProductUseCase } from "@/application/usecase/DeleteProduct";
 import { ProductRepository } from "@/application/repository/Product";
 import { EventPublisher } from "@/application/event/EventPublisher";
+import { ListProductsUseCase } from "@/application/usecase/ListProducts";
 
 export class ProductController {
-
   static registerRoutes(
-    httpServer: HttpServer, 
+    httpServer: HttpServer,
     productRepository: ProductRepository,
-    eventPublisher: EventPublisher,
+    eventPublisher: EventPublisher
   ) {
-  const createProductUseCase = new CreateProductUseCase(productRepository, eventPublisher)
-  const updateProductUseCase = new UpdateProductUseCase(productRepository, eventPublisher)
-  const deleteProductUseCase = new DeleteProductUseCase(productRepository, eventPublisher)
+    const listProductsUseCase = new ListProductsUseCase(productRepository,);
+    const createProductUseCase = new CreateProductUseCase(productRepository, eventPublisher);
+    const updateProductUseCase = new UpdateProductUseCase(productRepository, eventPublisher);
+    const deleteProductUseCase = new DeleteProductUseCase(productRepository, eventPublisher);
 
-    httpServer.post("/products", request => {
+    httpServer.get("/products", (request) => {
+      return listProductsUseCase.execute({ category: request.params.category });
+    });
+
+    httpServer.post("/products", (request) => {
       return createProductUseCase.execute({
         name: request.body.name,
         description: request.body.description,
         price: request.body.price,
-        category: request.body.category
-      })
-    })
-    httpServer.patch("/products/:id", request => {
+        category: request.body.category,
+      });
+    });
+
+    httpServer.patch("/products/:id", (request) => {
       return updateProductUseCase.execute({
         productId: request.params.id,
         name: request.body.name,
         description: request.body.description,
         price: request.body.price,
-      })
-    })
-    httpServer.delete("/products/:id", request => {
-      return deleteProductUseCase.execute({ productId: request.params.id })
-    })
+      });
+    });
+
+    httpServer.delete("/products/:id", (request) => {
+      return deleteProductUseCase.execute({ productId: request.params.id });
+    });
   }
 }
